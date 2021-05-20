7cae7f3eedff0d090df608fee110706d2e60a836
Nelwin Catalogo <catalogonelwin@gmail.com>
2021-05-20 23:19:57 +0800

    ✨ Implement Twin.Macro

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import GlobalStyles from "styles/GlobalStyles";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
