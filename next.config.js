e770b1d2afbdbe35fd4a02cc050dd619f73ba085
Nelwin Catalogo <catalogonelwin@gmail.com>
2022-01-03 14:33:54 +0800

    📦 Update nextjs to v12

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Unset client-side javascript that only works server-side
-      // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
-      config.node = { fs: "empty", module: "empty" };
-    }
-
+    if (!isServer) config.resolve.fallback.fs = false;
     return config;
   },
 };
